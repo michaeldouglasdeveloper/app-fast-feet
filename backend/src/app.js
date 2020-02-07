567980cd974787f3032616957cd30243c331a783
Michael Douglas Soares <soares.michael@gruposervices.com.br>
2020-02-07 11:07:50 -0300

    editor config and prettierrc

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,2 +1,22 @@
-import app from './server';
-app.listen(3001);
+import express from 'express';
+
+import routes from './routes';
+
+class App {
+
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,2 @@
-import express from 'express';
-
-import routes from './routes';
-
-class App {
-
-  constructor() {
-    this.server = express();
-    this.middlewares();
-    this.routes();
-  }
-
-  middlewares() {
-    this.server.use(express.json());
-  }
-
-  routes() {
-    this.server.use(routes);
-  }
-}
-
-export default new App().server;
+import app from './app';
+app.listen(3001);
